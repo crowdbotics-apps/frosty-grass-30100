cc122334528c6955902a83531e4fefb53938697c
Crowdbotics <team@crowdbotics.com>
2021-08-25 17:10:06 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "frosty_grass_30100Identifier",
+  url: "https://frosty_grass_30100Identifier.botics.co",
+  api: "https://frosty_grass_30100Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
